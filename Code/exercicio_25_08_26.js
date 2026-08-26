2824c1a6821ec993b341afcc244442afe905f5d2
Joao Victor <joaov282005@gmail.com>
2026-08-25 22:59:52 -0300

    função de Callback implementada!

--- Code/exercicio_25_08_26.js
+++ Code/exercicio_25_08_26.js
@@ -1,13 +1,36 @@
-function rand(min = 1, max = 6) {
+function rand(min = 1000, max = 6000) {
   let calc = Math.random() * (max - min) + min;
   return calc;
 }
-setTimeout(function () {
-  console.log("valor 1");
-}, rand());
-setTimeout(function () {
-  console.log("valor 2");
-}, rand());
-setTimeout(function () {
-  console.log("valor 3");
-}, rand());
+function f1(back) {
+  setTimeout(function () {
+    console.log("valor 1");
+    if (back) {
+      back();
+    }
+  }, rand());
+}
+function f2(back) {
+  setTimeout(function () {
+    console.log("valor 2");
+    if (back) {
+      back();
+    }
+  }, rand());
+}
+function f3(back) {
+  setTimeout(function () {
+    console.log("valor 3");
+    if (back) {
+      back();
+    }
+  }, rand());
+}
+
+f1(function () {
+  f2(function () {
+    f3(function () {
+      console.log("olá mundo");
+    });
+  });
+});
