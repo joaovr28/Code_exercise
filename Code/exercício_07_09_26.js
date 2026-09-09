dcf5b0c0c1dc13210ba507ce7badb5d17282e5f2
Joao Victor <joaov282005@gmail.com>
2026-09-09 20:06:02 -0300

    Create exercício_07_09_26.js

---
+++ Code/exercício_07_09_26.js
@@ -0,0 +1,9 @@
+let arrayteste = [2, 5, 1, 123, 64, 2, 3, 42, 47, 12, 31, 23, 96, 5];
+let newarray = [];
+
+for (let value of arrayteste) {
+  if (value >= 10) console.log('true') ; console.log('false');
+   
+  
+}
+console.log(newarray);
